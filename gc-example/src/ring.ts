b546a8cc6d284bfb4c4ccec191bf54d1a4458f2d
mpaulson <the.primeagen@gmail.com>
2023-10-07 08:25:03 -0600

    feat: gc and set

---
+++ gc-example/src/ring.ts
@@ -0,0 +1,10 @@
+import data from "./data.json";
+const {
+    low,
+    high,
+    style,
+} = data.ring;
+
+class Pool<T> {
+    constructor
+}
---
+++ set/src/index.ts
@@ -0,0 +1,108 @@
+import data from "./data.json";
+
+let {
+    count,
+    runCount,
+    charsUntil,
+} = data;
+
+if (process.argv[2]) {
+    count = +process.argv[2];
+}
+
+const str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$+[{(&=)}]*!|`%#\\";
+console.log("max", str.length);
+
+function createString(count: number, charsUntil: number) {
+    let substr = str.substring(0, count - 1);
+    let noRepeat = str.substring(0, count);
+
+    let i = 0;
+
+    return function() {
+        i++;
+        if (i < charsUntil) {
+            return substr[i % (count - 1)];
+        } else {
+            return noRepeat[i % count];
+        }
+    };
+}
+
+function findWithSet(iter: () => string) {
+    let set = new Set()
+    let runs = 0;
+    let resets = 0;
+    let sizeOnReset = 0;
+
+    while (set.size < count) {
+        runs++;
+        const char = iter();
+        const len = set.size;
+
+        set.add(char);
+        if (set.size === len) {
+            sizeOnReset += set.size;
+            resets++;
+            set = new Set();
+        }
+    }
+
+    return runs;
+}
+
+function findWithArray(iter: () => string) {
+    let arr: string[] = []
+    let runs = 0;
+    let resets = 0;
+    let sizeOnReset = 0;
+
+    while (arr.length < count) {
+        runs++;
+        const char = iter();
+        if (arr.includes(char)) {
+            sizeOnReset += arr.length;
+            arr = [];
+            resets++;
+        }
+
+        arr.push(char);
+    }
+
+    return runs;
+}
+
+function run(fn: (gen: () => string) => number): [number, number][] {
+    const runs: [number, number][] = [];
+
+    for (let i = 0; i < runCount; ++i) {
+        const gen = createString(count, charsUntil);
+        const start = Date.now();
+        const c = fn(gen);
+        const runtime = Date.now() - start;
+        runs.push([runtime, c]);
+    }
+
+    return runs;
+}
+
+function round(x: number) {
+    return Math.round(x * 1000) / 1000;
+}
+
+function printStats(name: string, values: [number, number][]) {
+    const [time, count] = values.reduce<[number, number]>((acc, [time, count]) => {
+        acc[0] += time;
+        acc[1] += count;
+        return acc;
+    }, [0, 0]);
+
+    console.log(name, "total time", time, "total runs", count, "runs / time", round(count / time));
+}
+
+function runAll() {
+    printStats("set", run(findWithSet));
+    printStats("arr", run(findWithArray));
+}
+
+runAll();
